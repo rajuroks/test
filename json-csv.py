83931c42fe4fe38fccd32f2c409e49cd1cb43bdd
rajuroks <raju.roks@gmail.com>
2024-10-06 20:48:59 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -7,3 +7,13 @@ Proactively identified critical Common Vulnerabilities and Exposures (CVEs), ens
 Key Role in Establishing the Vulnerability Management Program
 
 Played a pivotal role in the development and implementation of the vulnerability management program, which addressed a significant gap in the firm’s cloud security infrastructure. This program contributed to the reduction of the majority of critical vulnerabilities, substantially decreasing risk across cloud environments.
+
+
+
+  Demonstrates a high level of accountability by owning tasks and outcomes, consistently ensuring that actions and decisions contribute to the success of the team and the organization. Always takes responsibility for driving initiatives to completion while maintaining transparency.
+Contributes to an Inclusive and Diverse Work Environment
+
+Actively fosters a culture of inclusion by promoting diverse perspectives, ensuring all voices are heard, and creating an environment where team members feel valued. Encourages collaboration and open dialogue, supporting a team dynamic that embraces diversity.
+Treats Others with Respect and Acts as a Role Model for the Firm’s Culture
+
+Consistently treats colleagues with respect, promoting a professional and positive work environment. Leads by example, embodying the firm’s values and serving as a role model for upholding the culture. Strives to create a supportive and respectful workplace where others can thrive.
