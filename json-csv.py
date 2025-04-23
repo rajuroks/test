7b920b91018f2af6739f8bedbb8ee2efa0e4a171
rajuroks <raju.roks@gmail.com>
2025-04-22 22:15:35 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,1 +1,35 @@
-sdfs
+SELECT DISTINCT
+    n.id AS notification_id,
+    rro.eonid,
+    n.remediation_due_date,
+    n.added as notification_sent_date,
+    COUNT(rr.id) AS resource_count
+FROM
+    vm_data.cloudplatform_notification n
+LEFT OUTER JOIN
+    vm_data.cloudplatform_vulnerabilitynotification AS vn ON vn.notification_id = n.id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_vulnerabilityfindingworkload AS vfw ON vn.vulnerability_finding_id = vfw.vulnerability_finding_id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_workload AS w ON vfw.workload_id = w.id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_resource AS rwl ON w.resource_id = rwl.id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_ownership AS rwlo ON rwl.ownership_id = rwlo.id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_resource AS rr ON vfw.vulnerability_id = rr.id
+LEFT OUTER JOIN
+    vm_data.cloudplatform_ownership AS rro ON rr.ownership_id = rro.id
+WHERE
+    n.added IS NOT NULL
+    AND n.added >= '2024-01-01'  -- You can adjust this date as needed
+    AND n.added < '2024-04-01'   -- You can adjust this date as needed
+    AND rro.eonid IN ('')
+    AND rr.last_seen > n.remediation_due_date
+GROUP BY
+    n.id,
+    rro.eonid,
+    n.remediation_due_date,
+    n.added
+ORDER BY
+    n.id;
