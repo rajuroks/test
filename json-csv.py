9b0087053d3a8170f3f3a259811273013ae2baa5
rajuroks <raju.roks@gmail.com>
2025-04-24 13:01:23 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,14 +1,1 @@
-SELECT 
-  DATE_PART('day', added - first_seen) AS days_old,
-  COUNT(*) AS finding_count
-FROM 
-  cloudplatform_vulnerabilityfinding
-WHERE 
-  added BETWEEN '2025-04-01' AND '2025-04-23'
-  AND firstseen < added
-  AND DATE_PART('day', added - firstseen) >= 6
-GROUP BY 
-  days_old
-ORDER BY 
-  days_old;
 
