7185a49690456428e9d3b15894ece83fec9ce91b
rajuroks <raju.roks@gmail.com>
2025-04-25 13:54:28 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,1 +1,14 @@
 
+SELECT 
+    r.id AS container_id,
+    r.name AS container_name,
+    cl.id AS cluster_id,
+    cl.name AS cluster_name
+FROM 
+    cloudplatform_resource r
+LEFT JOIN 
+    cloudplatform_resource cl ON r.parent_id = cl.id AND cl.type = 'CL'
+WHERE 
+    r.type = 'CN'
+    AND r.provider = 'OP'
+LIMIT 20;
