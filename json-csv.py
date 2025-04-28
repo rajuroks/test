e841b204a86e72935b49332f49da9ae445d56374
rajuroks <raju.roks@gmail.com>
2025-04-28 12:09:54 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,1 +1,23 @@
-
+SELECT 
+    wl.id AS workload_id,
+    wl.first_seen AS workload_first_seen_date,
+    wl.added AS workload_added_date,
+    DATE_PART('day', wl.added - wl.first_seen) AS workload_days_old,
+    r.name AS container_resource_name,
+    i.name AS image_name,
+    cl.name AS cluster_name
+FROM 
+    cloudplatform_vulnerabilityfindingworkload vf
+JOIN 
+    cloudplatform_workload wl ON vf.workload_id = wl.id
+JOIN 
+    cloudplatform_resource r ON wl.resource_id = r.id
+LEFT JOIN 
+    cloudplatform_resource cl ON r.parent_id = cl.id AND cl.type = 'CL'
+JOIN 
+    cloudplatform_image i ON wl.image_id = i.id
+WHERE 
+    vf.added >= CURRENT_DATE - INTERVAL '4 days' -- recent finding
+    AND wl.first_seen < CURRENT_DATE - INTERVAL '4 days' -- workload existed before
+ORDER BY 
+    workload_days_old DESC;
