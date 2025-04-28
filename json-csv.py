79ea55c5077ed3b0c670a1566278f9f7c147a9d4
rajuroks <raju.roks@gmail.com>
2025-04-28 17:28:51 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,1 +1,34 @@
 
+SELECT 
+    CASE WHEN vf.added < '2025-04-20' THEN 'Before 04/20' ELSE 'From 04/20' END AS period,
+    COUNT(*) AS finding_count
+FROM 
+    cloudplatform_vulnerabilityfindingworkload vf
+JOIN 
+    cloudplatform_workload wl ON vf.workload_id = wl.id
+JOIN 
+    cloudplatform_resource r ON wl.resource_id = r.id
+WHERE 
+    r.provider = 'OP'
+GROUP BY 
+    period;
+
+
+
+SELECT 
+    CASE 
+        WHEN wl.first_seen >= '2025-04-20' THEN 'New Workloads'
+        ELSE 'Old Workloads'
+    END AS workload_type,
+    COUNT(*) AS finding_count
+FROM 
+    cloudplatform_vulnerabilityfindingworkload vf
+JOIN 
+    cloudplatform_workload wl ON vf.workload_id = wl.id
+JOIN 
+    cloudplatform_resource r ON wl.resource_id = r.id
+WHERE 
+    r.provider = 'OP'
+    AND vf.added >= '2025-04-20'
+GROUP BY 
+    workload_type;
