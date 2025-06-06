50789337e675bb5f09b9b0f7ed24b3f02fda3f7f
rajuroks <raju.roks@gmail.com>
2025-06-06 10:24:14 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,8 +1,1 @@
-SELECT COUNT(DISTINCT vulnerability_finding_id) AS recent_vulnerability_count
-FROM *
-WHERE 
-    provider = 'OP'
-    AND false_positive = FALSE
-    AND resource_lifespan > INTERVAL '24' HOUR
-    AND notification_date IS NOT NULL
-    AND vulnerability_first_seen >= CURRENT_DATE - INTERVAL '5' DAY;
+
