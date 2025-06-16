285fbc00abb7666f4a59688c1d289225a420a318
rajuroks <raju.roks@gmail.com>
2025-06-16 17:04:01 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,17 +1,1 @@
 
-SELECT
-  COUNT(CASE 
-          WHEN vulnerability_last_seen <= notification_date + INTERVAL '3 days' 
-          THEN 1 
-        END) AS remediated_count,
-
-  COUNT(CASE 
-          WHEN vulnerability_last_seen > notification_date + INTERVAL '3 days' 
-          THEN 1 
-        END) AS outstanding_count
-
-FROM (
-  SELECT vulnerability_last_seen, notification_date
-  FROM your_view_name
-  WHERE notification_date BETWEEN '2025-06-01' AND '2025-06-15'
-) sub;
