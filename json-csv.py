f90b9771236a647fe2d9b275d314c0713c16e3df
rajuroks <raju.roks@gmail.com>
2025-08-11 11:40:57 -0400

    Update json-csv.py

--- json-csv.py
+++ json-csv.py
@@ -1,10 +1,1 @@
-SELECT 
-    owner,
-    cve,
-    COUNT(DISTINCT notification_id) AS notification_count
-FROM cloudplatform_vmarc_vulnerability_details
-WHERE cve IN ('CVE-2024-1111', 'CVE-2024-2222', 'CVE-2024-3333') -- Your CVEs
-  AND notification_date >= DATE '2024-01-01'
-  AND notification_date <= CURRENT_DATE
-GROUP BY owner, cve
-ORDER BY owner, cve;
+
